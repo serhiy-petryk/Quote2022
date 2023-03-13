23964a3a25a9b7fc2b1988e9efde6352d70b142a
serhiy-petryk <sergei_petrik@yahoo.com>
2023-03-13 16:16:55 +0200

    Moved project from project-2020

---
+++ Quote2022/Models/ProfileYahoo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Quote2022.Models
+{
+    public class ProfileYahoo
+    {
+        public string Symbol;
+        public string Exchange;
+        public string Name;
+        public string Type;
+        public string Sector;
+        public string Industry;
+        public int? Employees;
+        public string Category;
+        public string Family;
+        public float? NetAssets;
+        public float? YtdDailyTotalReturn;
+        public float? Yield;
+        public string LegalType;
+        public DateTime TimeStamp;
+    }
+}

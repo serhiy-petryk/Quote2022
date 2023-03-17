61f8ef008fb44cab1dfa3bc825a95c2d8f8f90df
serhiy-petryk <sergei_petrik@yahoo.com>
2023-03-17 19:04:14 +0200

    Update Logger.cs

--- Data/Helpers/Logger.cs
+++ Data/Helpers/Logger.cs
@@ -9,7 +9,7 @@ namespace Data.Helpers
         public delegate void MessageAddedEventHandler(object sender, MessageAddedEventArgs e);
         public static event MessageAddedEventHandler MessageAdded;
 
-        private static readonly List<Tuple<DateTime, string, string>> Messages = new List<Tuple<DateTime, string, string>>();
+        private static readonly List<MessageAddedEventArgs> Messages = new List<MessageAddedEventArgs>();
 
         public static void AddMessage(string message)
         {
@@ -17,20 +17,19 @@ namespace Data.Helpers
             var method = sf.GetMethod();
             var callMethodName = method.DeclaringType?.Name + "." + method.Name;
 
-            var item = new Tuple<DateTime, string, string>(DateTime.Now, message, callMethodName);
-            Messages.Add(item);
-            MessageAdded?.Invoke(null, new MessageAddedEventArgs(item.Item1, item.Item2, item.Item3));
+            var oMessage = new MessageAddedEventArgs(message, callMethodName);
+            Messages.Add(oMessage);
+            MessageAdded?.Invoke(null, oMessage);
         }
 
         public class MessageAddedEventArgs : EventArgs
         {
-            public DateTime Date;
-            public string MethodName;
-            public string Message;
+            public readonly DateTime Date = DateTime.Now;
+            public readonly string MethodName;
+            public readonly string Message;
             public string FullMessage => $"{MethodName}. {Message}";
-            public MessageAddedEventArgs(DateTime date, string msg, string methodName)
+            public MessageAddedEventArgs(string msg, string methodName)
             {
-                Date = date;
                 Message = msg;
                 MethodName = methodName;
             }

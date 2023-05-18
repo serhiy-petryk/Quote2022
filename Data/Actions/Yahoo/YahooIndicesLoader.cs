00629fe5db132fd208144abf56c5c69203130c5d
serhiy-petryk <sergei_petrik@yahoo.com>
2023-05-18 09:35:09 +0300

    Update YahooIndicesLoader.cs

--- Data/Actions/Yahoo/YahooIndicesLoader.cs
+++ Data/Actions/Yahoo/YahooIndicesLoader.cs
@@ -36,68 +36,57 @@ namespace Data.Actions.Yahoo
             var to = GetYahooTime(DateTime.Now);
 
             var timeStamp = CsUtils.GetTimeStamp();
-            var folder = $@"E:\Quote\WebData\Daily\Yahoo\Indices\YahooIndices_{timeStamp.Item2}\";
+            var entries = new List<VirtualFileEntry>();
+            var data = new List<DayYahoo>();
 
             // Download data
             foreach (var symbol in Symbols)
             {
                 Logger.AddMessage($"Download data for {symbol}");
                 var url = string.Format(UrlTemplate, from, to, symbol);
-                var filename = $"{folder}{symbol}_{timeStamp.Item2}.csv";
-                Helpers.Download.DownloadToFile(url, filename);
-            }
+                var o = Download.DownloadToString(url);
+                if (o is Exception ex)
+                    throw new Exception($"YahooIndicesLoader.Start. Error while download from {url}. Error message: {ex.Message}");
 
-            // Parse and save data to database
-            Logger.AddMessage($"Parse and save files to database");
-            var data = new List<DayYahoo>();
-            foreach(var filename in Directory.GetFiles(folder, "*.csv"))
-                Parse(filename, data);
+                var lines = ((string)o).Split('\n');
+                if (lines.Length == 0)
+                    throw new Exception($"Invalid Day Yahoo quote file (no text lines): {o}");
+                if (lines[0] != "Date,Open,High,Low,Close,Adj Close,Volume")
+                    throw new Exception($"Invalid YahooIndex quotes file (check file header): {lines[0]}");
+
+                for (var k = 1; k < lines.Length; k++)
+                {
+                    if (!String.IsNullOrEmpty(lines[k].Trim()))
+                    {
+                        if (lines[k].Contains("null"))
+                            Debug.Print($"{symbol}, {lines[k]}");
+                        else
+                        {
+                            var ss = lines[k].Split(',');
+                            var item = new DayYahoo(symbol, ss);
+                            data.Add(item);
+                        }
+                    }
+                }
+            }
 
             if (data.Count > 0)
             {
-                Helpers.DbUtils.ClearAndSaveToDbTable(data, "Bfr_DayYahooIndexes", "Symbol", "Date", "Open", "High",
-                    "Low", "Close", "Volume", "AdjClose");
-                Helpers.DbUtils.ExecuteSql("INSERT into DayYahooIndexes (Symbol, Date, [Open], High, Low, [Close], Volume, AdjClose) " +
-                                           "SELECT a.Symbol, a.Date, a.[Open], a.High, a.Low, a.[Close], a.Volume, a.AdjClose " +
-                                           "from Bfr_DayYahooIndexes a " +
-                                           "left join DayYahooIndexes b on a.Symbol = b.Symbol and a.Date = b.Date " +
-                                           "where b.Symbol is null");
+                DbUtils.ClearAndSaveToDbTable(data, "Bfr_DayYahooIndexes", "Symbol", "Date", "Open", "High", "Low",
+                    "Close", "Volume", "AdjClose");
+                DbUtils.ExecuteSql("INSERT into DayYahooIndexes (Symbol, Date, [Open], High, Low, [Close], Volume, AdjClose) " +
+                                   "SELECT a.Symbol, a.Date, a.[Open], a.High, a.Low, a.[Close], a.Volume, a.AdjClose " +
+                                   "from Bfr_DayYahooIndexes a " +
+                                   "left join DayYahooIndexes b on a.Symbol = b.Symbol and a.Date = b.Date " +
+                                   "where b.Symbol is null");
 
                 Logger.AddMessage($"Update trading days");
-                Helpers.DbUtils.RunProcedure("pRefreshTradingDays");
+                DbUtils.RunProcedure("pRefreshTradingDays");
             }
-
-            // remove text files
-            Directory.Delete(folder, true);
 
             Logger.AddMessage($"!Finished. Last trade date: {data.Max(a => a.Date):yyyy-MM-dd}");
 
             long GetYahooTime(DateTime dt) => Convert.ToInt64((dt - new DateTime(1970, 1, 1)).TotalSeconds + 18000);
-        }
-
-        private static void Parse(string filename, List<DayYahoo> data)
-        {
-            var lines = File.ReadAllLines(filename);
-            if (lines.Length == 0)
-                throw new Exception($"Invalid Day Yahoo quote file (no text lines): {filename}");
-            if (lines[0] != "Date,Open,High,Low,Close,Adj Close,Volume")
-                throw new Exception($"Invalid YahooIndex quote file (check file header): {filename}");
-            var symbol = Path.GetFileNameWithoutExtension(filename).Split('_')[0];
-
-            for (var k = 1; k < lines.Length; k++)
-            {
-                if (!String.IsNullOrEmpty(lines[k].Trim()))
-                {
-                    if (lines[k].Contains("null"))
-                        Debug.Print($"{symbol}, {lines[k]}");
-                    else
-                    {
-                        var ss = lines[k].Split(',');
-                        var item = new DayYahoo(symbol, ss);
-                        data.Add(item);
-                    }
-                }
-            }
         }
     }
 }

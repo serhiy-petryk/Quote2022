b953fcd4c123fb2443527ad08b6f58b4e1b9c891
serhiy-petryk <sergei_petrik@yahoo.com>
2023-05-30 08:16:10 +0300

    x

--- Data/Actions/Yahoo/YahooMinuteQuotesLoader.cs
+++ Data/Actions/Yahoo/YahooMinuteQuotesLoader.cs
@@ -13,15 +13,29 @@ namespace Data.Actions.Yahoo
 
         public static void Start()
         {
-            /*var previousFriday = CsUtils.GetPreviousWeekday(DateTime.Now, DayOfWeek.Friday);
-            var previousMonday = previousFriday.AddDays(-4);
-            Start(previousMonday, 5, GetDefaultYahooSymbolList());*/
-            var previousThursday = CsUtils.GetPreviousWeekday(DateTime.Now, DayOfWeek.Thursday);
-            var previousMonday = previousThursday.AddDays(-3);
-            Start(previousMonday, 5, GetDefaultYahooSymbolList());
+            var from = DateTime.MaxValue;
+            var to = DateTime.MinValue;
+            using (var conn = new SqlConnection(Settings.DbConnectionString))
+            using (var cmd = conn.CreateCommand())
+            {
+                conn.Open();
+                cmd.CommandTimeout = 150;
+                cmd.CommandText = "SELECT min(date) MinDate, max(date) MaxDate FROM TradingDays where Date >= DATEADD(day, -7, GetDate())";
+                using (var rdr = cmd.ExecuteReader())
+                    while (rdr.Read())
+                    {
+                        from = (DateTime)rdr["MinDate"];
+                        to = (DateTime)rdr["MaxDate"];
+                    }
+            }
+
+            if (from < DateTime.MaxValue)
+                Start(from, to.AddDays(1), GetDefaultYahooSymbolList());
+            else
+                Logger.AddMessage($"!Finished. No data");
         }
 
-        public static void Start(DateTime from, int days, ICollection<string> yahooSymbols)
+        public static void Start(DateTime from, DateTime to, ICollection<string> yahooSymbols)
         {
             Logger.AddMessage($"Started");
 
@@ -30,7 +44,7 @@ namespace Data.Actions.Yahoo
             var errorFileName = $@"{Path.GetDirectoryName(zipFileName)}\DownloadErrors_{timeStamp.Item2}.txt";
 
             var fromInSeconds = GetYahooDate(from);
-            var toInSeconds = GetYahooDate(from.AddDays(days));
+            var toInSeconds = GetYahooDate(to);
 
             var cnt = 0;
             var downloadErrors = new List<string>();
@@ -38,7 +52,7 @@ namespace Data.Actions.Yahoo
             foreach (var symbol in yahooSymbols)
             {
                 cnt++;
-                if (cnt%10 == 0)
+                if (cnt % 10 == 0)
                     Logger.AddMessage($@"Downloaded {cnt:N0} files from {yahooSymbols.Count:N0}");
 
                 var url = string.Format(UrlTemplate, symbol, fromInSeconds, toInSeconds);
@@ -47,7 +61,7 @@ namespace Data.Actions.Yahoo
                     downloadErrors.Add($"{symbol}\t{ex.Message}");
                 else
                 {
-                    var entry = new VirtualFileEntry($@"YahooMinute_{timeStamp.Item2}\yMin-{symbol}.txt", (string) o);
+                    var entry = new VirtualFileEntry($@"YahooMinute_{timeStamp.Item2}\yMin-{symbol}.txt", (string)o);
                     virtualFileEntries.Add(entry);
                 }
 
@@ -87,13 +101,13 @@ namespace Data.Actions.Yahoo
             using (var cmd = conn.CreateCommand())
             {
                 conn.Open();
-                cmd.CommandText = "SELECT '^DJI' Symbol UNION SELECT '^GSPC' UNION "+
-                                  "SELECT b.YahooSymbol FROM dbQuote2022..DayEoddata a "+
-                                  "INNER JOIN dbQuote2022..SymbolsEoddata b on a.Exchange = b.Exchange and a.Symbol = b.Symbol "+
-                                  "WHERE b.YahooSymbol is not null AND a.volume* a.[close]>= 5000000 and a.date >= DATEADD(day, -30, GetDate()) UNION "+
-                                  "SELECT b.Symbol from dbQuote2022..SymbolsEoddata a "+
-                                  "RIGHT JOIN(SELECT * from dbQuote2022..ScreenerNasdaqStock "+
-                                  "WHERE Deleted is null or Deleted > DATEADD(day, -30, GetDate())) b "+
+                cmd.CommandText = "SELECT '^DJI' Symbol UNION SELECT '^GSPC' UNION " +
+                                  "SELECT b.YahooSymbol FROM dbQuote2022..DayEoddata a " +
+                                  "INNER JOIN dbQuote2022..SymbolsEoddata b on a.Exchange = b.Exchange and a.Symbol = b.Symbol " +
+                                  "WHERE b.YahooSymbol is not null AND a.volume* a.[close]>= 5000000 and a.date >= DATEADD(day, -30, GetDate()) UNION " +
+                                  "SELECT b.Symbol from dbQuote2022..SymbolsEoddata a " +
+                                  "RIGHT JOIN(SELECT * from dbQuote2022..ScreenerNasdaqStock " +
+                                  "WHERE Deleted is null or Deleted > DATEADD(day, -30, GetDate())) b " +
                                   "ON a.NasdaqSymbol = b.Symbol WHERE a.Symbol is null AND b.MaxTradeValue > 5 ORDER BY 1";
 
                 using (var rdr = cmd.ExecuteReader())

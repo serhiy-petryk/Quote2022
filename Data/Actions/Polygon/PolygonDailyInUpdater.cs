8f8445be743ea5207cb6af29ec00ba178147b6a1
serhiy-petryk <sergei_petrik@yahoo.com>
2023-12-11 21:53:24 +0200

    Create PolygonDailyInUpdater.cs

---
+++ Data/Actions/Polygon/PolygonDailyInUpdater.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace Data.Actions.Polygon
+{
+    public class PolygonDailyInUpdater
+    {
+        public static void Run()
+        {
+            using (var conn = new SqlConnection(Settings.DbConnectionString))
+            using (var cmd = conn.CreateCommand())
+            {
+                conn.Open();
+                cmd.CommandText = "SELECT a.Symbol, a.Date from DayPolygon a "+
+                                  "left join DayPolygonIn b on a.Symbol = b.Symbol and a.Date = b.Date "+
+                                  "where a.TradeCount >= 5000 and a.Volume* a.[Close]/ 1000000 > 10 and b.Symbol is null "+
+                                  "order by a.Date, a.Symbol";
+
+                using (var rdr = cmd.ExecuteReader())
+                    while (rdr.Read())
+                        Update((string)rdr["Symbol"], (DateTime)rdr["Date"]);
+            }
+        }
+
+        private static void Update(string symbol, DateTime date)
+        {
+
+        }
+
+    }
+}
